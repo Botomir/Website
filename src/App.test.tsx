0ed895cfba56edef031da1bc7af2ac12ca7ce9fd
SoorajModi <soomodi@gmail.com>
2021-05-13 01:29:31 -0400

    Add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,32 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+beforeEach(() => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+});
+
+test('renders Documentation link', () => {
+  const linkElement = screen.getByText(/Docs/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Statistics link', () => {
+  const linkElement = screen.getByText(/Stats/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders About link', () => {
+  const linkElement = screen.getByText(/About/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders Add to Server button', () => {
+  const linkElement = screen.getByText(/Add To Server/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Donate button', () => {
+  const linkElement = screen.getByText(/Donate/i);
   expect(linkElement).toBeInTheDocument();
 });
