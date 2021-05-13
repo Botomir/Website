175a0f710e5c94aed29a768e0b7647b1d6ffae6d
SoorajModi <soomodi@gmail.com>
2021-05-13 01:58:04 -0400

    Add router and pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import About from './about';
+import Docs from './docs';
 import Home from './home';
+import NotFound from './notFound';
+import Statistics from './statistics';
 
-export default Home;
+export {
+  About,
+  Docs,
+  Home,
+  NotFound,
+  Statistics,
+};
---
+++ src/pages/statistics/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Statistics() {
+  return (
+    <p>Statistics page</p>
+  );
+}
+
+export default Statistics;
