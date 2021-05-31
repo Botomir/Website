50cccbff274f60996f9ba6983a40a0730b92c42e
SoorajModi <soomodi@gmail.com>
2021-05-30 20:22:21 -0400

    Add aboutBotomir component

---
+++ src/pages/about/aboutBotomir.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function AboutBotomir() {
+  return (
+    <Description>
+      Botomir is a Discord Bot written in Javascript. They were born on November 25, 2020, and was a
+      happy accident more than a planned project. Botomir was created because he is a remedy to loneliness. It
+      can be just you and Botomir, they won&apos;t tell. Botomir loves to eat chicken and waffles, washing it down
+      with a Four loco, and cookies for dessert. Their favourite song is the windows startup sound. Their
+      hobbies include going on long scrolls through Wikipedia, watching the sunset on YouTube, and snuggling
+      up with the CPU on a cold night. Botomir&apos;s favourite tv show is Mr. Robot, and their favourite movie is
+      2001: A Space Odyssey. Botomir also loves to play. Feel free to ask them a command, they don&apos;t wear a
+      shirt, but still has plenty of tricks up their sleeve. Botomir likes it when you call them a good bot,
+      and also kinda likes it when you call them a bad bot. Botomir would love to be a part of your server and improve
+      your server and Discord experience.
+    </Description>
+  );
+}
+
+const Description = styled.p`
+    font-size: 22px;
+    margin-top: 0;
+    margin-bottom: 0;
+    padding-top: 5vh;
+    padding-bottom: 5vh;
+    line-height: 1.5;
+`;
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Container } from '@material-ui/core';
+import styled from 'styled-components';
+import AboutBotomir from './aboutBotomir';
 
-function About() {
+export default function About() {
   return (
-    <p>About page</p>
+    <Container>
+      <Title>About Botomir</Title>
+      <AboutBotomir />
+      <SubTitle>Meet the Devs</SubTitle>
+    </Container>
   );
 }
 
-export default About;
+const Title = styled.p`
+    font-size: 64px;
+    margin-top: 5vh;
+    margin-bottom: 0;
+    font-weight: 300;
+    text-align: center;
+`;
+
+const SubTitle = styled.p`
+    font-size: 36px;
+    font-weight: 300;
+    margin-top: 0;
+`;
