faace7e30ac45e96cb13655a42260009fcdf6854
Sooraj <soomodi@gmail.com>
2021-08-07 20:43:40 -0400

    Footer (#20)

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import { StylesDictionary } from '../../types/StylesDictionary';
+import { FaGithub } from 'react-icons/fa';
+import styled from 'styled-components';
 
 export default function Footer() {
   return (
-    <div className="footer" style={styles.footer}>
-      <p style={styles.footerText}>Footer</p>
-    </div>
+    <FooterContainer>
+      <Link href="https://github.com/Botomir" target="_blank">
+        <FaGithub size={25} />
+      </Link>
+    </FooterContainer>
   );
 }
 
-const styles: StylesDictionary = {
-  footer: {
-    background: '#0F5376',
-    paddingTop: '1.0rem',
-    paddingBottom: '1.0rem',
-    position: 'absolute',
-    bottom: '0',
-    left: '0',
-    right: '0',
-    width: '100%',
-    textAlign: 'center',
-    display: 'block',
-    height: '2.5rem',
-  },
-  footerText: {
-    color: 'white',
-  },
-};
+const FooterContainer = styled.div`
+  background: #0F5376;
+  padding-top: 1.0rem;
+  padding-bottom: 1.0rem;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  display: block;
+`;
+
+const Link = styled.a`
+  color: white;
+`;
